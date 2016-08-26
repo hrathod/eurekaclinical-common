958e0c4e0d50c6dd7412aeacfe1f62b751453bc9
Andrew Post <arpost@emory.edu>
2016-08-26 01:41:36 -0400

    Constrain the domain of postMessages.

--- src/main/java/org/eurekaclinical/common/servlet/PostMessageLoginServlet.java
+++ src/main/java/org/eurekaclinical/common/servlet/PostMessageLoginServlet.java
@@ -23,12 +23,14 @@ package org.eurekaclinical.common.servlet;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import javax.inject.Inject;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import javax.inject.Singleton;
+import org.eurekaclinical.standardapis.props.CasEurekaClinicalProperties;
 
 
 /**
@@ -40,11 +42,19 @@ import javax.inject.Singleton;
 @Singleton
 public class PostMessageLoginServlet extends HttpServlet {
 
+    private final CasEurekaClinicalProperties properties;
+    
+    @Inject
+    public PostMessageLoginServlet(CasEurekaClinicalProperties inProperties) {
+        this.properties = inProperties;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
         try (PrintWriter out = new PrintWriter(new OutputStreamWriter(resp.getOutputStream()))) {
             out.println("<html><head></head><body><script type=\"text/javascript\">");
-            out.println("window.parent.postMessage('OK', '*');");
+            out.println("parent.postMessage('OK', '" + this.properties.getDomainUrl() + "');");
             out.println("</script></body></html>");
         }
     }
