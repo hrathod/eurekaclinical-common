02cedef488d2f5d6a8ccb51de53029fdd5ef2911
Andrew Post <arpost@emory.edu>
2017-08-26 20:25:05 -0400

    Fixed resource leak when Tomcat cleans up expired sessions.

--- src/main/java/org/eurekaclinical/common/config/ClientSessionListener.java
+++ src/main/java/org/eurekaclinical/common/config/ClientSessionListener.java
@@ -19,7 +19,6 @@ package org.eurekaclinical.common.config;
  * limitations under the License.
  * #L%
  */
-
 import com.google.inject.ConfigurationException;
 import com.google.inject.Injector;
 import javax.servlet.ServletContext;
@@ -30,62 +29,82 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Manages the lifecycle of a session-scoped REST API client. Invoke it as
- * follows, where <code>Client</code> is the name of a subclass of
- * {@link EurekaClinicalClient}:
- * 
+ * Manages the lifecycle of a Eureka! Clinical REST API client that is bound in
+ * Guice with session scope. Use it as follows, where <code>Client</code> is 
+ * the name of a subclass of {@link EurekaClinicalClient}:
+ *
  * <pre>
  * servletContext.addSessionListener(new ClientSessionListener(Client.class));
  * </pre>
- * 
+ *
  * @author Andrew Post
  */
 public class ClientSessionListener implements HttpSessionListener {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ClientSessionListener.class);
 
     private final Class<? extends EurekaClinicalClient> clientCls;
-    
+    private EurekaClinicalClient client;
+
     /**
-     * Creates a listener for instances of the specified client class.
-     * 
-     * @param inClientCls the client class.
+     * Creates a listener for instances of the specified session-scoped client 
+     * class.
+     *
+     * @param inClientCls the client class. Cannot be <code>null</code>.
      */
     public ClientSessionListener(Class<? extends EurekaClinicalClient> inClientCls) {
+        if (inClientCls == null) {
+            throw new IllegalArgumentException("inClientCls cannot be null");
+        }
         this.clientCls = inClientCls;
     }
 
     /**
-     * Just logs the creation of the session.
+     * Gets the client instance from Guice and stores it so that we can close 
+     * it when the session is destroyed.
      * 
+     * We cannot just get the client instance in {@link #sessionDestroyed(javax.servlet.http.HttpSessionEvent) }
+     * because it may be called from outside of session scope. We have observed
+     * this happening when Tomcat sweeps its list of sessions to determine if
+     * any of them are expired and can be destroyed.
+     *
      * @param hse the session event.
      */
     @Override
     public void sessionCreated(HttpSessionEvent hse) {
-        LOGGER.info("Creating session for client {} for service {}", 
-                this.clientCls.getName(), 
-                hse.getSession().getServletContext().getContextPath());
+        ServletContext servletContext = hse.getSession().getServletContext();
+        LOGGER.info("Creating client {} for service {}",
+                this.clientCls.getName(),
+                servletContext.getContextPath());
+        Injector injector = (Injector) servletContext.getAttribute(Injector.class.getName());
+        try {
+            this.client = injector.getInstance(this.clientCls);
+        } catch (ConfigurationException ce) {
+            LOGGER.error("Error creating client "
+                    + this.clientCls.getName()
+                    + " for service "
+                    + servletContext.getContextPath(), ce);
+        }
     }
 
     /**
-     * Logs the destruction of the session and attempts to close the client.
-     * Logs any error in closing the client.
-     * 
+     * Attempts to close the client.
+     *
      * @param hse the session event.
      */
     @Override
     public void sessionDestroyed(HttpSessionEvent hse) {
         ServletContext servletContext = hse.getSession().getServletContext();
-        LOGGER.info("Destroying session for client {} for service {}", 
-                this.clientCls.getName(), 
-                servletContext.getContextPath());
-        Injector injector = (Injector) servletContext.getAttribute(Injector.class.getName());
-        try {
-            injector.getInstance(this.clientCls).close();
-        } catch (ConfigurationException ce) {
-            LOGGER.error("Error destroying session for client " + 
-                    this.clientCls.getName() + 
-                    " for service " + 
-                    servletContext.getContextPath(), ce);
+        if (this.client != null) {
+            LOGGER.info("Destroying client {} for service {}",
+                    this.clientCls.getName(),
+                    servletContext.getContextPath());
+
+            this.client.close();
+        } else {
+            LOGGER.warn("The client {} for service {} was not created at the start of the session, so there is nothing to close.",
+                    this.clientCls.getName(),
+                    servletContext.getContextPath());
         }
     }
 }
